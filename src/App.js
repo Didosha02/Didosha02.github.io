0ca5e4f7c59185ba773f457f99c630986d67066e
Didar <sagiakil2002@gmail.com>
2022-04-08 00:01:08 +0600

    Lab-9

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+function App() {
+return (
+  
+<div className='page'>
+  <div className='header-1'>
+    <img src="https://chocolife.object.pscloud.io/static/angular/assets/icons/logo_new.svg"/> 
+    <span className='gorod'> <img src="https://cdn-icons-png.flaticon.com/512/17/17736.png"/>Алматы</span>
+    <button className='login'>Войти</button>
+    <div className='search'><h1 className='item'>ищу...</h1></div>
+    </div>
+</div>
+);
+}
+export default App;
